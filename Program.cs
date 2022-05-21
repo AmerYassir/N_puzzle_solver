bc8a35c09f04ab4a1712e4139b13873d40d198ff
yoka47961 <94013110+yoka47961@users.noreply.github.com>
2022-05-21 12:44:16 +0200

    update bonus

--- Program.cs
+++ Program.cs
@@ -45,33 +45,90 @@ namespace N_puzzle_cs
         {
             int[]BFS_arr=new int[size*size];
             for (int i = 0; i < size; i++)
-            {
                 for (int j = 0; j < size; j++)
-                {
                     BFS_arr[i * size + j] = input[i, j];
-                }
-            }
+
+
+
             BFS bfs=new BFS();
-            Node firstNode=new Node(BFS_arr,size);
-            List<Node> output= bfs.Breadth(firstNode);
-            for (int i = 0; i < output.Count; i++)
+            Puzzle firstNode =new Puzzle(BFS_arr,size);
+            List<Puzzle> output= bfs.Breadth(firstNode);
+            int count = 1;
+            Console.WriteLine();
+            Console.WriteLine(" Puzzle");
+            output[output.Count - 1].showHowSolvePuzzle();
+            Console.WriteLine();
+            Console.WriteLine(" Solve puzzle");
+            for (int i = output.Count - 2; i >= 0; i--)
             {
-                Console.WriteLine("move " + (i + 1));
-                output[i].printPuzzle();
+                Console.WriteLine();
+                Console.Write("  Move #" + count);
+                Console.WriteLine();
+                output[i].showHowSolvePuzzle();
+                count++;
+               
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Total # of movements = " + (output.Count - 1));
+        }
+        static public void start(int[,] input)
+        {
+
+            Console.WriteLine("                    ******************************************************************************");
+            Console.WriteLine("                    ******************************************************************************");
+            Console.WriteLine("                    **                                                                          **");
+            Console.WriteLine("                    **                                                                          **");
+            Console.WriteLine("                    **                                                                          **");
+            Console.WriteLine("                    **                                                                          **");
+            Console.WriteLine("                    **                                                                          **");
+            Console.WriteLine("                    **                           Welcome to N puzzle                            **");
+            Console.WriteLine("                    **                                                                          **");
+            Console.WriteLine("                    **                                                                          **");
+            Console.WriteLine("                    **                                                                          **");
+            Console.WriteLine("                    **                                                                          **");
+            Console.WriteLine("                    **                                                                          **");
+            Console.WriteLine("                    ******************************************************************************");
+            Console.WriteLine("                    ******************************************************************************");
+            Console.WriteLine();
+
+            int Choose;
+            Console.WriteLine("How do you want to solve the puzzle?");
+            Console.WriteLine("1. solve by A* algorithm");
+            Console.WriteLine("2. solve by BFS algorithm");
+            Console.WriteLine("------------------------------------------------------------------------");
+            Console.Write("Enter the number of the solution method you want to solve the puzzle: ");
+            Choose = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("------------------------------------------------------------------------");
+
+            if (Choose == 1)
+            {
+                AS_Code(input, input.GetLength(0), 0);
+            }
+            else if (Choose == 2)
+            {
+                BFS_Code(input, ((input.GetLength(0))));
             }
         }
         static void Main()
         {
             //paths for tets folders
-            string ComMTest = "C:\\Users\\Amer\\source\\repos\\N_puzzle_cs\\Testcases\\Complete\\Complete Test\\Solvable puzzles\\Manhattan Only\\";
+            string ComMTest = "C:\\Users\\Amer\\source\\repos\\N_puzzle_cs\\Testcases\\Sample\\Sample Test\\Solvable Puzzles\\";
+            //string ComMTest = "C:\\Users\\ayamo\\Documents\\GitHub\\N_puzzle_solver\\Testcases\\Sample\\Sample Test\\Solvable Puzzles\\";
             string ComMaHTest = "C:\\Users\\Amer\\source\\repos\\N_puzzle_cs\\Testcases\\Complete\\Complete Test\\Solvable puzzles\\Manhattan & Hamming\\";
+            //string ComMaHTest = "C:\\Users\\ayamo\\Documents\\GitHub\\N_puzzle_solver\\Testcases\\Complete\\Complete Test\\Solvable puzzles\\Manhattan & Hamming\\";
             // file name in the used folder , dont forget to type .txt at the end
-            string fileName = "15 Puzzle 1.txt";
+            string fileName = "8 Puzzle (1).txt";
             int[,] input = TakeInput(ComMTest + fileName);
 
             // use on of these algorithms
             // BFS_Code(input,((input.GetLength(0))));
             // AS_Code(input, input.GetLength(0), 0);
+
+            start(input);
+
+           
+
         }
     }
 }
---
+++ Puzzle.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_puzzle_cs
+{
+    internal class Puzzle
+    {
+		public Puzzle basicPuzzle;
+		public List<Puzzle> childPazzle = new List<Puzzle>();
+		public List<int> puzzleGame = new List<int>();
+
+		public Puzzle(int[] puzzle, int sizePuzzle)
+		{
+			for (int i = 0; i < sizePuzzle * sizePuzzle; i++)
+				if (puzzle[i] == 0)
+					this.puzzleGame.Add(puzzle.Length);
+				else
+					this.puzzleGame.Add(puzzle[i]);
+		}
+		public void up(int index, List<int> puzzle, int sizePuzzle)
+		{
+			int[] matCopy = new int[sizePuzzle * sizePuzzle];
+			if (0 <= index - sizePuzzle)
+			{
+				//Copy mat
+				for (int i = 0; i < sizePuzzle * sizePuzzle; i++)
+					matCopy[i] = puzzle[i];
+
+				//Swap 
+				int temp = matCopy[index - sizePuzzle];
+				matCopy[index - sizePuzzle] = matCopy[index];
+				matCopy[index] = temp;
+
+				Puzzle nodeChild = new Puzzle(matCopy, sizePuzzle);
+				childPazzle.Add(nodeChild);
+				nodeChild.basicPuzzle = this;
+
+			}
+		}
+		public void down(int index, List<int> puzzle, int sizePuzzle)
+		{
+			int[] matCopy = new int[sizePuzzle * sizePuzzle];
+			if (sizePuzzle * sizePuzzle > index + sizePuzzle)
+			{
+				//Copy mat
+				for (int i = 0; i < sizePuzzle * sizePuzzle; i++)
+					matCopy[i] = puzzle[i];
+
+				//Swap 
+				int temp = matCopy[index + sizePuzzle];
+				matCopy[index + sizePuzzle] = matCopy[index];
+				matCopy[index] = temp;
+
+				Puzzle nodeChild = new Puzzle(matCopy, sizePuzzle);
+				childPazzle.Add(nodeChild);
+				nodeChild.basicPuzzle = this;
+
+			}
+		}
+		public void right(int index, List<int> puzzle, int sizePuzzle)
+		{
+			if (sizePuzzle - 1 > index % sizePuzzle)
+			{
+				int[] matCopy = new int[sizePuzzle * sizePuzzle];
+
+				//Copy mat
+				for (int i = 0; i < sizePuzzle * sizePuzzle; i++)
+					matCopy[i] = puzzle[i];
+
+				//Swap 
+				int temp = matCopy[index + 1];
+				matCopy[index + 1] = matCopy[index];
+				matCopy[index] = temp;
+
+				Puzzle nodeChild = new Puzzle(matCopy, sizePuzzle);
+				childPazzle.Add(nodeChild);
+				nodeChild.basicPuzzle = this;
+
+			}
+		}
+		public void left(int index, List<int> puzzle, int sizePuzzle)
+		{
+			int[] matCopy = new int[sizePuzzle * sizePuzzle];
+			if (0 < index % sizePuzzle)
+			{
+				//Copy mat
+				for (int i = 0; i < sizePuzzle * sizePuzzle; i++)
+					matCopy[i] = puzzle[i];
+
+				//Swap 
+				int temp = matCopy[index - 1];
+				matCopy[index - 1] = matCopy[index];
+				matCopy[index] = temp;
+
+				Puzzle nodeChild = new Puzzle(matCopy, sizePuzzle);
+				childPazzle.Add(nodeChild);
+				nodeChild.basicPuzzle = this;
+
+			}
+		}
+		public bool testPuzzleIsGoal()
+		{
+			int element = puzzleGame[0];
+			bool goal = false;
+			for (int i = 1; i < puzzleGame.Count; i++)
+			{
+				if (puzzleGame[i] < element)
+					goal = true;
+				element = puzzleGame[i];
+			}
+			return goal;
+		}
+		public void movePiece()
+		{
+			for (int i = 0; i < puzzleGame.Count; i++)
+				if (puzzleGame[i] == puzzleGame.Count)
+				{
+					up(i, puzzleGame, (int)Math.Sqrt(puzzleGame.Count));
+					down(i, puzzleGame, (int)Math.Sqrt(puzzleGame.Count));
+					right(i, puzzleGame, (int)Math.Sqrt(puzzleGame.Count));
+					left(i, puzzleGame, (int)Math.Sqrt(puzzleGame.Count));
+					break;
+				}
+		}
+		public bool puzzleRepeat(List<int> puzzle)
+		{
+			bool check = false;
+			for (int i = 0; i < puzzle.Count; i++)
+				if (puzzleGame[i] != puzzle[i])
+					check = true;
+
+			return check;
+		}
+		public void showHowSolvePuzzle()
+		{
+			int index = 0;
+			for (int i = 0; i < (int)Math.Sqrt(puzzleGame.Count); i++)
+			{
+				for (int j = 0; j < (int)Math.Sqrt(puzzleGame.Count); j++)
+				{
+					if (puzzleGame[index] == puzzleGame.Count)
+						Console.Write("  " + " ");
+					else
+						Console.Write("  " + puzzleGame[index]);
+					index++;
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
